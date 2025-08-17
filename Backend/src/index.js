b132b12b80ec096c6d3b4ab44e642e42ab79e1ff
Gaurav Rajput <er.gaurav2022@gmail.com>
2025-08-17 15:57:04 +0530

    utils for async function handler api error api response

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,9 +1,65 @@
-import express from "express";
+// require('dotenv').config({path:'./env'})
+import dotenv from "dotenv";//-r dotenv/config --experimental-json-modules only for this import if uper require then no need
+import connectDB from "./db/db.js";
+import { app } from "./app.js";
+dotenv.config({
+    path:'./env'
+})
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`Server is running at port: ${process.env.PORT}`)
+    })
+})
+.catch((err)=>{
+    console.log("Mongo db connection failed !!",err);
+})
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 const app=express();
-app.get("/api/src",(req,res)=>{
-    res.send("hello")
-});
-app.listen(3000,()=>{
-    console.log("server is running")
-});
+(async()=>{
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        app.on("error",(error)=>{
+            console.log(error);
+            throw error;
+        })
+        app.listen(process.env.PORT,()=>{
+            console.log("server is running")
+        });
+    } catch (error) {
+        console.error(error);
+        throw error
+    }
+})()*/
