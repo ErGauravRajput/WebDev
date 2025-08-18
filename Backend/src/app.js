ce722d0b6356e71e27fdf65c0553842743c85484
Gaurav Rajput <er.gaurav2022@gmail.com>
2025-08-18 12:43:07 +0530

    add register controller

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -7,13 +7,14 @@ app.use(cors({
     origin:process.env.CORS_ORIGIN,
     credentials:true
 }));
-
 app.use(express.json({limit:"16kb"}));
 // app.use(express.urlencoded());
 app.use(express.urlencoded({extended:true,limit:"16kb"}));
-
 app.use(express.static("public"));
-
 app.use(cookieParser());
 
+//routes import
+import userRouter from "./routes/user.routes.js";
+//router declaration
+app.use("/api/v1/users",userRouter);
 export {app};
